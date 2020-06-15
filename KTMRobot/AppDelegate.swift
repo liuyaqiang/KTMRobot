4a0312725646321aa7d1ebfa1f0c2dce4b99cd70
liuyaqiang <344257448@qq.com>
2020-06-15 18:44:14 +0800

    #KTMRobot

---
+++ KTMRobot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KTMRobot
+//
+//  Created by liuyaqiang on 2020/6/15.
+//  Copyright © 2020 KTM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KTMRobot/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  KTMRobot
+//
+//  Created by liuyaqiang on 2020/6/15.
+//  Copyright © 2020 KTM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func press(_ sender: Any) {
+        
+        print("123")
+    }
+    
+}
+
